2a5e9903ffc4e82c4c488e4a68a4a7edb3615e58
Gagan <gagansasan91@yahoo.com>
2023-11-22 12:12:42 -0700

    backend code added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+require('./db/config.js');
+const User = require('./db/User.js');
 
 const app = express();
- app.get('/',(req, resp)=>{
-    resp.send("Your app is working!!")
- });
-
+app.use(express.json())
+app.use(cors())
+app.post("/register", async (req, res) => {
+   try {
+       const user = new User(req.body);
+       const result = await user.save();
+       console.warn(result)
+       res.send(result);
+   } catch (error) {
+       res.status(500).send({ error: error.message });
+   }
+});
  app.listen(5000)
