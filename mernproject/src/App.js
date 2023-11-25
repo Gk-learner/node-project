f19e1425d63fd0aa7c8ed8f5cab649b7d7bbb391
Gagan <gagansasan91@yahoo.com>
2023-11-25 00:27:20 -0700

    login logout code

--- mernproject/src/App.js
+++ mernproject/src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Footer from './components/Footer';
 import SignUp from './components/Signup';
-// import PrivateComponent from './components/PrivateComponent'
+import PrivateComponent from './components/PrivateComponet'
 import Login from './components/Login'
 import AddProduct from './components/AddProduct';
 import ProductList from './components/ProductList';
@@ -15,13 +15,14 @@ function App() {
      <BrowserRouter >
       <Nav />
      <Routes>
-       {/* <Route element={<PrivateComponent />} /> */}
+
+       <Route element={<PrivateComponent />} >
        <Route path="/" element={<ProductList />} />
        <Route path="/add" element={<AddProduct />} />
        <Route path="/update/:id" element={<UpdateProduct />} />
        <Route path="/logout" element={<h1> Logout Component</h1>} />
        <Route path="/profile" element={<h1>Profile Component</h1>} />
-       {/* </Route> */}
+       </Route>
 
        <Route path="/signup" element={<SignUp />} />
        <Route path="/login" element={<Login />} />
